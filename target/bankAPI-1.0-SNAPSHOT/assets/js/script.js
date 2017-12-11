44ac4d3e358d276fc6886fbb919d1277bed94844
Kamil <kamil.lasecki@eircom.net>
2017-12-11 15:51:10 +0000

    Revert "addef registration form validation"
    This reverts commit d4fc4bece3d649787aea59790b32937a4b1f6e11.

--- target/bankAPI-1.0-SNAPSHOT/assets/js/script.js
+++ target/bankAPI-1.0-SNAPSHOT/assets/js/script.js
@@ -1,42 +1,37 @@
 (function() {
     $(document).ready(function() {
+        $("#btn_send").click(function(){
+            var user = new Object();
+            var address = new Object();
 
-        $('#form').validator().on('submit', function (e) {
-            if (e.isDefaultPrevented()) {
-                console.log("noooo: ");
-            } else {
-                var user = new Object();
-                var address = new Object();
+            address.street = $('#input_address').val();
+            address.city = $('#input_city').val();
+            address.county = $('#input_county').val();
 
-                address.street = $('#input_address').val();
-                address.city = $('#input_city').val();
-                address.county = $('#input_county').val();
-
-                user.name = $('#input_name').val();
-                user.email = $('#input_email').val();
-                user.address = address;
-                user.login = $('#input_username').val();
-                user.password = $('#input_password').val();
+            user.name = $('#input_name').val();
+            user.email = $('#input_email').val();
+            user.address = address;
+            user.login = $('#input_username').val();
+            user.password = $('#input_password').val();
 
 
-                $.ajax({
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    url: "/api/users",
-                    type: "POST",
-                    data: JSON.stringify(user),
-                    dataType: "json",
-                    success: function (result) {
-                        console.log("successfully sent" + result );
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        console.log("ERROR: " + textStatus, errorThrown);
-                    }
-                });
-                console.log("sending : " + user)
-            }
-        })
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+                url: "/api/users",
+                type: "POST",
+                data: JSON.stringify(user),
+                dataType: "json",
+                success: function (result) {
+                    console.log("successfully sent" + result );
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log("ERROR: " + textStatus, errorThrown);
+                }
+            });
+        console.log("sending : " + user)
+        });
     })
 }());
